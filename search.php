5140ccca65aa8f30d06f9a09b7ca29cb7e7cbdff
Serena Wang <serenaw1@andrew.cmu.edu>
2018-03-25 15:56:50 -0400

    Add search results template
    We now have a template for search results so that our search results show up on
    a reasonable page. I also removed unnecessary code from singular.php that was
    copied over from index.php but wasn't relevant to the functionality of
    singular.php.

--- search.php
+++ search.php
@@ -21,9 +21,7 @@
           <a href="https://www.cmu.edu/"><img class="cmu-logo" src="<?php echo get_template_directory_uri() . '/img/cmu_wordmark.png';?>"></a>
         </div>
         <a href="<?php echo home_url(); ?>" style="color: inherit;"><h1 class="wscs-info">WOMEN<b>@SCS</b></h1></a>
-        <!--p class="wscs-info hide-on-small-screens"> The Women@SCS mission is to create, encourage, and support academic, social, and professional opportunities for women in computer science and to promote the breadth of the field and its diverse community. </p-->
       </div>
-      <!-- end W@SCS general info (CMU logo, W@SCS, mission) -->
 
       <div class="col s12 section-divider"><hr></div>
 
@@ -32,15 +30,27 @@
 
   <div class="col s12 section-divider"></div>
   <div class="wscs-page-content">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php if ( have_posts() ) : ?>
+      <header class="search-page-header">
+        <span class="page-title">
+          <?php printf( "<b>Search Results for \"" . get_search_query() . "\"</b>"); ?>
+        </span>
+      </header>
 
-      <div class="page-title "><b><?php the_title() ?></b></div>
-      <p class="description"><?php the_content() ?></p>
+      <?php while ( have_posts() ) : the_post(); ?>
+        <div class="search-post">
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          <?php the_excerpt(); ?>
+        </div>
+      <?php endwhile; ?>
 
-    <?php endwhile; else : ?>
-
-      <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-
+    <?php else : ?>
+      <header class="search-page-header">
+        <span class="page-title">
+          <?php printf( "<b>Search Results for \"" . get_search_query() . "\"</b>"); ?>
+        </span>
+      </header>
+      <p class="no-results"><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
     <?php endif; ?>
   </div>
 </main>
